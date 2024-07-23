18c6431bde3d0b2c4597840b8cce66f165dfb88d
Henrique Santiago <henrique_santiago93@hotmail.com>
2024-07-23 18:52:14 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,31 @@
+Container for generating a Docker container for running Dropbox.
+
+References:
+
+https://github.com/otherguy/docker-dropbox
+
+# Build all images
+
+
+```
+docker build --no-cache -t dropbox .
+```
+
+# Tag and push it
+
+```
+docker tag dropbox wingeon/dropbox
+docker push wingeon/dropbox
+```
+
+# Running
+
+```
+docker network create dropbox
+
+docker run --log-opt max-size=10m --log-opt max-file=10 \
+	-d -it --restart=always --name=dropbox_mydrop --net="dropbox" \
+	-e "TZ=$(readlink /etc/localtime | sed 's#^.*/zoneinfo/##')" \
+	-e "DROPBOX_UID=$(id -u)" -e "DROPBOX_GID=$(id -g)" -e "POLLING_INTERVAL=20" \
+	-v "~/dropboxes/mydrop:/opt/dropbox/Dropbox" dropbox
+```
---
+++ docker-entrypoint.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# Set TZ if not provided with enviromental variable.
+if [ -z "${TZ}" ]; then
+  export TZ="$(cat /etc/timezone)"
+else
+  if [ ! -f "/usr/share/zoneinfo/${TZ}" ]; then
+      echo "The timezone '${TZ}' is unavailable!"
+      exit 1
+  fi
+
+  echo "${TZ}" > /etc/timezone
+  ln -fs "/usr/share/zoneinfo/${TZ}" /etc/localtime
+fi
+
+# Set UID/GID if not provided with enviromental variable(s).
+if [ -z "${DROPBOX_UID}" ]; then
+  export DROPBOX_UID=$(/usr/bin/id -u dropbox)
+  echo "DROPBOX_UID not specified, defaulting to dropbox user id (${DROPBOX_UID})"
+fi
+
+if [ -z "${DROPBOX_GID}" ]; then
+  export DROPBOX_GID=$(/usr/bin/id -g dropbox)
+  echo "DROPBOX_GID not specified, defaulting to dropbox user group id (${DROPBOX_GID})"
+fi
+
+# Look for existing group, if not found create dropbox with specified GID.
+if [ -z "$(grep ":${DROPBOX_GID}:" /etc/group)" ]; then
+  usermod -g users dropbox
+  groupdel dropbox
+  groupadd -g $DROPBOX_GID dropbox
+fi
+
+if [[ ! "${POLLING_INTERVAL}" =~ ^[0-9]+$ ]]; then
+  echo "POLLING_INTERVAL not set to a valid number, defaulting to 5!"
+  export POLLING_INTERVAL=5
+fi
+
+# Set dropbox account's UID/GID.
+usermod -u ${DROPBOX_UID} -g ${DROPBOX_GID} --non-unique dropbox > /dev/null 2>&1
+
+# Change ownership to dropbox account on all working folders.
+if [[ $(echo "${SKIP_SET_PERMISSIONS:-false}" | tr '[:upper:]' '[:lower:]' | tr -d " ") == "true" ]]; then
+  echo "Skipping permissions check, ensure the dropbox user owns all files!"
+  chown ${DROPBOX_UID}:${DROPBOX_GID} /opt/dropbox
+else
+  chown -R ${DROPBOX_UID}:${DROPBOX_GID} /opt/dropbox
+fi
+
+# Change permissions on Dropbox folder
+chmod 755 /opt/dropbox/Dropbox
+
+#  Dropbox did not shutdown properly? Remove files.
+[ ! -e "/opt/dropbox/.dropbox/command_socket" ] || rm /opt/dropbox/.dropbox/command_socket
+[ ! -e "/opt/dropbox/.dropbox/iface_socket" ]   || rm /opt/dropbox/.dropbox/iface_socket
+[ ! -e "/opt/dropbox/.dropbox/unlink.db" ]      || rm /opt/dropbox/.dropbox/unlink.db
+[ ! -e "/opt/dropbox/.dropbox/dropbox.pid" ]    || rm /opt/dropbox/.dropbox/dropbox.pid
+
+# Update Dropbox to latest version unless DROPBOX_SKIP_UPDATE is set
+# Empty line
+echo ""
+
+# Set umask
+umask 002
+
+# Print timezone
+echo "Using $(cat /etc/timezone) timezone ($(date +%H:%M:%S) local time)"
+dpkg-reconfigure --frontend noninteractive tzdata
+
+# Start Dropbox
+echo "Starting dropboxd ($(cat /opt/dropbox/bin/VERSION))..."
+gosu dropbox "$@" & export DROPBOX_PID="$!"
+trap "/bin/kill -SIGQUIT ${DROPBOX_PID}" INT
+
+# Wait a few seconds for the Dropbox daemon to start
+sleep 30
+
+# Dropbox likes to restart itself. In that case, the container will exit!
+while kill -0 ${DROPBOX_PID} 2> /dev/null; do
+  [ -d "/proc/${DROPBOX_PID}" ] && [ -f "/opt/dropbox/.dropbox/info.json" ] && gosu dropbox dropbox status
+  /usr/bin/find /tmp/ -maxdepth 1 -type d -mtime +1 ! -path /tmp/ -exec rm -rf {} \;
+  /bin/sleep ${POLLING_INTERVAL}
+done
+
+gosu dropbox cp -r /opt/dropbox/Dropbox/.dropbox-dist/* /opt/dropbox/bin/
